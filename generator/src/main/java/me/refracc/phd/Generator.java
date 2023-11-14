dba1392071b9ce7fd0d54388f90cc8d7bad30ad4
refracc <40345422@live.napier.ac.uk>
2023-11-14 12:44:59 +0000

    begin work on generator

--- generator/src/main/java/me/refracc/phd/Generator.java
+++ generator/src/main/java/me/refracc/phd/Generator.java
@@ -1,6 +1,27 @@
 package me.refracc.phd;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+
 public class Generator {
 
-  
+  public static void main(String[] args) {
+    Generator gen = new Generator();
+
+    for (int i = 0; i < 100; i++) {
+      String problem = "p" + i + ".pddl";
+
+      try (BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(gen.getClass().getResourceAsStream("constants.txt"))))){
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  private static String problem(int i) {
+    return "(define (problem " + "p" + i + ") (:domain courses)";
+  }
 }
