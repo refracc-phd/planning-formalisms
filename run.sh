b50769e8a24e249067bb7400be1fec3c1bb35e4e
refracc <s.anderson.23@abdn.ac.uk>
2024-03-01 11:40:18 +0000

    add contrived domains

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 sbatch --parition=uoa-compute sh ./sokoban.sh
 sbatch --parition=uoa-compute sh ./blocks-strips.sh
+sbatch --parition=uoa-compute sh ./ma-sokoban.sh
+sbatch --parition=uoa-compute sh ./ma-blocks-strips.sh
