402f362cf84b43bc49ae5ca7cb3635033f34d45c
refracc <s.anderson.23@abdn.ac.uk>
2024-03-01 16:34:16 +0000

    last task for maxwell

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sbatch --partition=uoa-compute ./sokoban.sh
-sbatch --partition=uoa-compute ./blocks-strips.sh
+# sbatch --partition=uoa-compute ./sokoban.sh
+# sbatch --partition=uoa-compute ./blocks-strips.sh
 sbatch --partition=uoa-compute ./ma-sokoban.sh
-sbatch --partition=uoa-compute ./ma-blocks-strips.sh
+# sbatch --partition=uoa-compute ./ma-blocks-strips.sh
