b354cf6a2a80141922a9204adfbb67cd57baad29
refracc <s.anderson.23@abdn.ac.uk>
2024-03-19 13:32:06 +0000

    remove plots & update plot script to make text bigger

--- plots.r
+++ plots.r
@@ -1,31 +1,31 @@
 # Load required libraries
 library(ggplot2)
 
-setwd("~/Documents/PhD/planning-formalisms")
+setwd("~/Documents/planning-formalisms")
 
 df <- read.csv("output.csv")
 
 ground_actions <- ggplot(df, aes(x = domain, y = grounded.actions, fill = domain)) +
-  geom_boxplot(show.legend = FALSE) +
+  geom_boxplot() +
   labs(x = 'Domain', y = "Number of Grounded Actions [log10]", title = "Number of Grounded Actions for each Domain") +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   scale_y_log10()
 
 ground_actions
 
 ground_fluents <- ggplot(df, aes(x = domain, y = grounded.fluents, fill = domain)) +
-  geom_boxplot(show.legend = FALSE) +
+  geom_boxplot() +
   labs(x = 'Domain', y = "Number of Grounded Fluents [log10]", title = "Number of Grounded Fluents for each Domain")+
   theme(element_text(size = 18)) +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   scale_y_log10()
 
 ground_fluents
 
 ground_external_actions <- ggplot(df, aes(x = domain, y = grounded.external.actions, fill = domain)) +
-  geom_boxplot(show.legend = FALSE) +
+  geom_boxplot() +
   labs(x = 'Domain', y = "Number of Grounded External Actions [log10]", title = "Number of Grounded External Actions for each Domain") +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   scale_y_log10() +
   theme(element_text(size = 18))
 
@@ -55,7 +55,7 @@ initial_vs_actual_heuristic
 search_time <- ggplot(df, aes(x = domain, y = mean(search.time.msec), fill = domain), log10="y") +
   geom_col(show.legend = FALSE) +
   labs(x = "Domain", y = "Search Time (msec) [log10]", title = "Average Search Time per Domain") +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   theme(element_text(size = 18))
 
 search_time
@@ -63,23 +63,23 @@ search_time
 duplicates <- ggplot(df, aes(x = domain, y = duplicates.detected, fill = domain), log10="y") +
   geom_col(show.legend = FALSE) +
   labs(x = "Domain", y = "Duplicates Detected [log10]", title = "Number of Duplicates Detected per Domain") +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   theme(element_text(size = 18))
 
 duplicates
 
 plan_length <- ggplot(df, aes(x = domain, y = plan.length, fill = domain)) +
-  geom_col(show.legend = FALSE) +
+  geom_col() +
   labs(x = "Domain", y = "Plan Length [log10]", title = "Length of Plans per Domain") +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   theme(element_text(size = 18))
 
 plan_length
 
 grounding_time <- ggplot(df, aes(x = domain, y = mean(grounding.time), fill = domain)) +
-  geom_col(show.legend = FALSE) +
+  geom_col() +
   labs(x = "Domain", y = "Grounding Time (msec)", title = "Average Grounding Time per Domain") +
-  scale_x_discrete(labels = c("block-group", "bw", "continuous", "discrete", "farmland", "fn-cntrs", "fn-cntrs-inv", "fn-cntrs-rnd", "ma-bw", "ma-sban", "plnt-wtring", "sailing", "sban")) +
+  scale_x_discrete(labels = c("bg", "bw", "c", "d", "farm", "fn-cntrs", "fn-inv", "fn-rnd", "ma-bw", "ma-sban", "plant", "sail", "sban")) +
   theme(element_text(size = 18))
 
 grounding_time
